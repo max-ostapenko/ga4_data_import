41f56c5957ef0893e73a85ab56a783b1c33be137
max-ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2023-05-24 13:10:17 +0000

    bucket access completed

--- ga4_data_import/storage.py
+++ ga4_data_import/storage.py
@@ -1,20 +1,29 @@
 from google.cloud import storage
+from ga4_data_import.common import get_project_number
 
 
 def add_bucket_read_access(
-    project_id,
-    bucket_name
+    project_id: str,
+    bucket_name: str,
 ):
-    
+    """
+    Add read access to the bucket for the compute service account.
+
+    Args:
+        project_id: The project id.
+        bucket_name: The name of the bucket to add read access to.
+    """
     client = storage.Client()
     bucket = client.get_bucket(bucket_name)
     policy = bucket.get_iam_policy(requested_policy_version=3)
 
-    compute_service_account_email = f"{project_id}-compute@developer.gserviceaccount.com"
+    project_number = get_project_number(project_id)
     policy.bindings.append(
         {
             "role": "roles/storage.objectViewer",
-            "members": [f"serviceAccount:{compute_service_account_email}"],
+            "members": [
+                f"serviceAccount:{project_number}-compute@developer.gserviceaccount.com"
+            ],
         }
     )
     bucket.set_iam_policy(policy)
